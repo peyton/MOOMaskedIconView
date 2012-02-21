cd113ced123ae9e63f01f4529b7280813885a908
Peyton Randolph <pjrandolph@gmail.com>
2012-02-20 23:13:19 -0500

    Add appledoc

--- GOODMaskedIconView.h
+++ GOODMaskedIconView.h
@@ -19,14 +19,38 @@ typedef void (^DrawingBlock)(CGContextRef context);
     CGImageRef _mask;
 }
 
+/* @name Properties */
+
+/*
+ * Whether the icon view is in its highlighted state.
+ */
 @property (nonatomic, assign, getter = isHighlighted) BOOL highlighted;
 
+/*
+ * The color painted when the icon view is unhighlighted.
+ */
 @property (nonatomic, strong) UIColor *color;
+
+/*
+ * The color painted when the icon view is highlighted.
+ */
 @property (nonatomic, strong) UIColor *highlightedColor;
 
+/*
+ * A block called with the current context at the end of every drawing. Has signature `void (^DrawingBlock)(CGContextRef context)`.
+ *
+ * Useful for custom drawing.
+ */
 @property (nonatomic, strong) DrawingBlock drawingBlock;
+
+/*
+ * The current CGImage mask held by the view. Read-only.
+ *
+ * @see Configuration methods
+ */
 @property (nonatomic, assign, readonly) CGImageRef mask;
 
+/* @name Initialization methods */
 - (id)initWithImage:(UIImage *)image;
 - (id)initWithImage:(UIImage *)image size:(CGSize)size;
 - (id)initWithImageNamed:(NSString *)imageName;
@@ -36,6 +60,7 @@ typedef void (^DrawingBlock)(CGContextRef context);
 - (id)initWithResourceNamed:(NSString *)resourceName;
 - (id)initWithResourceNamed:(NSString *)resourceName size:(CGSize)size;
 
+/* @name Configuration methods */
 - (void)configureWithImage:(UIImage *)image;
 - (void)configureWithImage:(UIImage *)image size:(CGSize)size;
 - (void)configureWithImageNamed:(NSString *)imageName;
@@ -45,7 +70,24 @@ typedef void (^DrawingBlock)(CGContextRef context);
 - (void)configureWithResourceNamed:(NSString *)resourceName;
 - (void)configureWithResourceNamed:(NSString *)resourceName size:(CGSize)size;
 
+/* @name Rendering */
+
+/* 
+ * Render the icon unhighlighted to a UIImage.
+ * 
+ * Useful for passing the icon to other views, e.g. UIButton.
+ * 
+ * @see renderHighlightedImage
+ */
 - (UIImage *)renderImage;
+
+/*
+ * Render the icon highlighted to a UIImage.
+ * 
+ * Useful for passing the icon to other views, e.g. UIButton.
+ * 
+ * @see renderImage
+ */
 - (UIImage *)renderHighlightedImage;
 
 @end
