759215878978a15e1d29589d0210f026305df2c2
Peyton Randolph <pjrandolph@gmail.com>
2012-05-02 18:58:53 -0500

    Update appledoc

--- build-appledoc.sh
+++ build-appledoc.sh
@@ -2,5 +2,5 @@
 
 BASEDIR=$(dirname $0)
 
-appledoc -o "$BASEDIR" --project-name MOOMaskedIconView --project-company "Peyton Randolph" --company-id com.peyton.MOOMaskedIconView --create-html --no-repeat-first-par "$1"
+appledoc -o "$BASEDIR" --keep-intermediate-files --project-name MOOMaskedIconView --project-company "Peyton Randolph" --company-id com.peyton.MOOMaskedIconView --create-html --no-repeat-first-par --publish-docset --docset-feed-url http://peytn.com/MOOMaskedIconView/%DOCSETATOMFILENAME --docset-package-url http://peytn.com/MOOMaskedIconView/%DOCSETPACKAGEFILENAME --no-install-docset "$1"
 
