9fe6c9d1fe68e05b8d2126d21c3ffc937da30027
Peyton Randolph <pjrandolph@gmail.com>
2012-05-02 19:09:22 -0500

    Update appledoc

--- build-appledoc.sh
+++ build-appledoc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+VERSION="0.2"
 BASEDIR=$(dirname $0)
 
-appledoc -o "$BASEDIR" --keep-intermediate-files --project-name MOOMaskedIconView --project-company "Peyton Randolph" --company-id com.peyton.MOOMaskedIconView --create-html --no-repeat-first-par --publish-docset --docset-feed-url http://peytn.com/MOOMaskedIconView/%DOCSETATOMFILENAME --docset-package-url http://peytn.com/MOOMaskedIconView/%DOCSETPACKAGEFILENAME --no-install-docset "$1"
+appledoc -o "$BASEDIR" --project-version "$VERSION" --keep-intermediate-files --project-name MOOMaskedIconView --project-company "Peyton Randolph" --company-id com.peytn.MOOMaskedIconView --create-html --no-repeat-first-par --publish-docset --docset-feed-url http://peytn.com/MOOMaskedIconView/%DOCSETATOMFILENAME --docset-package-url http://peytn.com/MOOMaskedIconView/%DOCSETPACKAGEFILENAME "$1"
 
